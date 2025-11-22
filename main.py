2a89aa35f3529ab8597a09ea27ba072730f3f6af
Reece Crone <reece.crone@gmail.com>
2025-11-22 16:03:19 -0600

    created a 10 by 10 grid that changes the colors of cells when they are clicked and stores the cells data in 2d array

---
+++ main.py
@@ -0,0 +1,53 @@
+from PyQt6 import QtWidgets
+from ui_main_window import Ui_MainWindow
+import sys
+
+from PyQt6 import QtGui
+
+class MainApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Grid setup
+        self.rows = 10
+        self.cols = 10
+        self.ui.dungeonWidget.setRowCount(self.rows)
+        self.ui.dungeonWidget.setColumnCount(self.cols)
+        self.ui.dungeonWidget.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
+
+
+        for i in range(self.rows):
+            self.ui.dungeonWidget.setRowHeight(i, 60)
+        for j in range(self.cols):
+            self.ui.dungeonWidget.setColumnWidth(j, 60)
+
+        # Fill cells white initially
+        for i in range(self.rows):
+            for j in range(self.cols):
+                item = QtWidgets.QTableWidgetItem()
+                item.setBackground(QtGui.QColor("white"))
+                self.ui.dungeonWidget.setItem(i, j, item)
+
+        # Connect cell click
+        self.ui.dungeonWidget.cellClicked.connect(self.cell_clicked)
+
+        # Initialize data structure
+        self.cell_data = [["white" for _ in range(self.cols)] for _ in range(self.rows)]
+
+    # Update cell_clicked
+    def cell_clicked(self, row, col):
+        # Toggle color in data
+        current_color = self.cell_data[row][col]
+        new_color = "red" if current_color == "white" else "white"
+        self.cell_data[row][col] = new_color
+
+        # Update UI
+        self.ui.dungeonWidget.item(row, col).setBackground(QtGui.QColor(new_color))
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainApp()
+window.show()
+sys.exit(app.exec())
